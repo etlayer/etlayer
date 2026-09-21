c013a964ff98e78910d5a02020976520aca18206
Serhii Ponomarov <sergii.ponomarov@gmail.com>
2026-09-22 02:25:09 +0300

    feat: register identity linked contract

--- packages/cloudflare-ingest/contracts/index.js
+++ packages/cloudflare-ingest/contracts/index.js
@@ -1,11 +1,13 @@
 import accountCreatedV1 from "./account.created.v1.js";
 import heroCtaClickedV1 from "./landing.hero.cta_clicked.v1.js";
 import heroExposedV1 from "./landing.hero.exposed.v1.js";
+import identityLinkedV1 from "./identity.linked.v1.js";
 
 const CONTRACTS = [
   heroExposedV1,
   heroCtaClickedV1,
   accountCreatedV1,
+  identityLinkedV1,
 ];
 
 const CONTRACTS_BY_KEY = new Map(
